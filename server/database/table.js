0dba599ca2d41f054c03e284533f1dc3ebaeda92
WISDOM-WOKE22 <wisodmwokedev@gmail.com>
2023-06-25 15:22:11 +0100

    added new enpoints for the server and also changed the file structure

--- server/database/table.js
+++ server/database/table.js
@@ -1,18 +1,18 @@
 const sqlite = require('sqlite3').verbose();
 const db = new sqlite.Database(
-    './data.sqlite3',
+    './data.db',
     sqlite.OPEN_READWRITE, (err) => {
     if (err){
         return console.log(err);
     }
 })
 
-const user = `CREATE TABLE user(ID INT PRIMARY KEY, username, password)`;
+const user = `CREATE TABLE user(ID INT PRIMARY KEY, username UNIQUE, password)`;
 
-const pointOfInterext = `CREATE TABLE pointOfInterest (ID INT PRIMARY KEY, name VARCHAR(255), type VARCHAR(255), country VARCHAR(255), region VARCHAR(255), lon FLOAT, lat FLOAT, description TEXT, recommendations INT )`;
+const pointOfInterest = `CREATE TABLE poi (ID INT PRIMARY KEY, name VARCHAR(255), type VARCHAR(255), country VARCHAR(255), region VARCHAR(255), lon FLOAT, lat FLOAT, description TEXT, recommendations INT )`;
 
 const reviews = `CREATE TABLE reviews (ID INT PRIMARY KEY, poi_id INT, review TEXT)`;
 
-db.run(pointOfInterext)
+db.run(pointOfInterest)
 
 // module.exports = db;
